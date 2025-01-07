440342a91fd06d9585332d0cec26ea934d26f79f
Victor <victorclovisb@hotmail.com>
2025-01-06 21:40:17 -0300

    feat: Implement controllers end-points

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+  @Get()
+  findAll(@Res() response) {
+    return response.status(200).json({ message: 'Listagem de cursos' });
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Curso com ID: ${id}`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    console.log(body);
+
+    return `Atualiza Curso com ID: ${id}`;
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Deletado Curso com ID: ${id}`;
+  }
+}
