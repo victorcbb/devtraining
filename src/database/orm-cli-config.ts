3e22a4a8a7b9549c3d6e59960db8e67387bfc456
Victor <victorclovisb@hotmail.com>
2025-01-19 11:55:40 -0300

    feat: Create coursesTags table

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,9 +2,14 @@ import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1736717556091 } from 'src/migrations/1736717556091-CreateCoursesTable';
 import { CreateTagsTable1736815022387 } from 'src/migrations/1736815022387-CreateTagsTable';
+import { CreateCoursesTagsTable1737075840103 } from 'src/migrations/1737075840103-CreateCoursesTagsTable';
 
 export const AppDataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1736717556091, CreateTagsTable1736815022387],
+  migrations: [
+    CreateCoursesTable1736717556091,
+    CreateTagsTable1736815022387,
+    CreateCoursesTagsTable1737075840103,
+  ],
 });
