2c500bbd364557949fc0da57e74172ec9798d856
Victor <victorclovisb@hotmail.com>
2025-01-21 21:27:08 -0300

    feat: Add coursesId column to courses_tags table

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -3,6 +3,7 @@ import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1736717556091 } from 'src/migrations/1736717556091-CreateCoursesTable';
 import { CreateTagsTable1736815022387 } from 'src/migrations/1736815022387-CreateTagsTable';
 import { CreateCoursesTagsTable1737075840103 } from 'src/migrations/1737075840103-CreateCoursesTagsTable';
+import { AddCoursesIdToCoursesTags1737298462509 } from 'src/migrations/1737298462509-AddCoursesIdToCoursesTags';
 
 export const AppDataSource = new DataSource({
   ...dataSourceOptions,
@@ -11,5 +12,6 @@ export const AppDataSource = new DataSource({
     CreateCoursesTable1736717556091,
     CreateTagsTable1736815022387,
     CreateCoursesTagsTable1737075840103,
+    AddCoursesIdToCoursesTags1737298462509,
   ],
 });
